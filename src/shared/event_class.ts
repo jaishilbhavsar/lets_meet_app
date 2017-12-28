299330e0448b7d35fc1ab848dd3020b954c711f4
zeel91297 <zeel91297@gmail.com>
2017-12-28 11:38:58 +0530

    event create complete

--- src/shared/event_class.ts
+++ src/shared/event_class.ts
@@ -1,10 +1,13 @@
+import { DateTime } from "ionic-angular/components/datetime/datetime";
+
 export class Events_Class {
     constructor(public event_id: number,
         public event_name: string,
         public event_des: string,
         public event_pic: string,
-        public event_s_time: string,
-        public event_e_time: string,
+        public event_s_time: DateTime,
+        public event_e_time: DateTime,
+        public event_date:DateTime,
         public event_loc: string,
         public fk_user_id: string,
         public fk_comm_id: number,
