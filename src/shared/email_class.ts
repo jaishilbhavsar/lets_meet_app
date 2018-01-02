95ae78d252cd913a08c885f34e18ad1dd7af96f7
jaishilbhavsar <jaishilbhavsar@yahoo.in>
2018-01-02 15:12:49 +0530

    forget password done

--- src/shared/email_class.ts
+++ src/shared/email_class.ts
@@ -1,6 +1,6 @@
 export class email_class
 {
-    constructor(public msg:string,public email:string,public subject:string)
+    constructor(public message:string,public name:string,public subject:string)
     {
         
     }
