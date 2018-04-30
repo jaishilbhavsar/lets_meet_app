4735fa3450e89e818c6f4b339a01019b9a83b886
zeel91297 <zeel91297@gmail.com>
2018-04-30 20:22:38 +0530

    done

--- src/shared/category_class.ts
+++ src/shared/category_class.ts
@@ -1,23 +1,20 @@
-<<<<<<< HEAD
 import { DateTime } from "ionic-angular/components/datetime/datetime";
-export class Category_class
-{
-    constructor(public cat_id:number,
-                public cat_name:string,
-                public comm_id: number,
-                public comm_name: string,
-                public comm_des: string,
-                public comm_pic: string,
-                public comm_date: DateTime,
-                public comm_rating: number,
-                public created_by: string){
-                    
-                }
-=======
+export class Category_class {
+    constructor(public cat_id: number,
+        public cat_name: string,
+        public comm_id: number,
+        public comm_name: string,
+        public comm_des: string,
+        public comm_pic: string,
+        public comm_date: DateTime,
+        public comm_rating: number,
+        public created_by: string) {
+
+    }
+}
 export class Category_Class {
     constructor(public cat_id: number,
         public cat_name: string) {
 
     }
->>>>>>> 41551c0d1547f8a58104539fbe1a3ae26e0e0df7
 }
